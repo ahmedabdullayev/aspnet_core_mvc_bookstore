d84c867a194276f5cfe11e3396b6f43465832b05
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-10-18 20:04:31 +0300

    Dynamic book details page

--- BookStore/Repository/BookRepository.cs
+++ BookStore/Repository/BookRepository.cs
@@ -25,10 +25,25 @@ namespace BookStore.Repository
         {
             return new List<BookModel>()
             {
-                new BookModel() {Id = 1, Title = "MVC", Author = "Winny", Description = "Description for the MVC book"},
-                new BookModel() {Id = 2, Title = "C#", Author = "Winny", Description = "Description for the C# book"},
-                new BookModel() {Id = 3, Title = "Java", Author = "Peeter", Description = "Description for the Java book"},
-                new BookModel() {Id = 4, Title = "PHP", Author = "Monika", Description = "Description for the PHP book"},
+                new BookModel() { 
+                    Id = 1, Title = "MVC", Author = "Winny", Description = "Description for the MVC book", 
+                    Category = "Programming", Language = "English", TotalPages = 322
+                },
+                new BookModel()
+                {
+                    Id = 2, Title = "C#", Author = "Winny", Description = "Description for the C# book",
+                    Category = "Programming", Language = "Portugal", TotalPages = 765
+                },
+                new BookModel()
+                {
+                    Id = 3, Title = "Java", Author = "Peeter", Description = "Description for the Java book",
+                    Category = "Programming", Language = "Deutsch", TotalPages = 543
+                },
+                new BookModel()
+                {
+                    Id = 4, Title = "PHP", Author = "Monika", Description = "Description for the PHP book",
+                    Category = "Programming", Language = "Spanish", TotalPages = 313
+                },
             };
         }
     }
