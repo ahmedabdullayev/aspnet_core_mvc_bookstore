be476808c8f4c87cc891d26f3d49e3f9196c29e6
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-10-18 14:11:14 +0300

    Run time compilation added

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -20,6 +20,7 @@ namespace BookStore
         {
 
             services.AddControllersWithViews();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
