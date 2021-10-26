dfb1b4316d5988a0fce1bbc5958487b904205483
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-10-26 21:22:57 +0300

    ViewData attributes

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -6,16 +6,26 @@ namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string CustomProperty { get; set; }
+
+        [ViewData]
+        public string Title { get; set; }
+
+        [ViewData]
+        public BookModel Book { get; set; }
         public ViewResult Index()
         {
+            Title = "Home page";
+            CustomProperty = "Custom value";
+            Book = new BookModel() {Id = 32, Title = "Good book"};
             
-            ViewData["book"] = new BookModel() {Id = 5, Author = "Ahmed Abdullajev"};
-
             return View();
         }
 
         public ViewResult AboutUs()
         {
+            Title = "About Us";
             return View();
         }
     }
